97bd93864a738161d089555f9442cca6d0ff768e
pasha-log <pasha.log@gmail.com>
2023-03-14 16:54:56 -0700

    wrote express route for getting user info by username

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/** Routes for users. */
+
+const express = require('express');
+const { ensureCorrectUser } = require('../middleware/auth');
+// const { BadRequestError } = require('../expressError');
+const User = require('../models/user');
+
+const router = express.Router();
+
+/** GET /[username] => { user }
+ *
+ * Returns { username, fullName, email, profileImageURL, bio, posts, postLikes }
+ *   where posts is [{ postId, postURL, caption, watermark, filter, createdAt }, ...]
+ *   where postLikes is [{ postId, username, postURL, caption, watermark, filter, createdAt }, ...]
+ *   where following is [{username, fullName, profileImageURL}, ...]
+ *   where followers is [{username, fullName, profileImageURL}, ...]
+ *
+ * Authorization required: same user-as-:username
+ **/
+
+router.get('/:username', ensureCorrectUser, async function(req, res, next) {
+	try {
+		const user = await User.get(req.params.username);
+		return res.json({ user });
+	} catch (err) {
+		return next(err);
+	}
 });
 
 module.exports = router;
