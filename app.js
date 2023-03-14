80a61578d927212cf460768df178e0fa7df3a068
pasha-log <pasha.log@gmail.com>
2023-03-14 16:44:51 -0700

    edited the app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const authRoutes = require('./routes/auth');
+const usersRoutes = require('./routes/users');
 
 var app = express();
 
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRoutes);
+app.use('/users', usersRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
