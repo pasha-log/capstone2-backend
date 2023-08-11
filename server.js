490b03b6be827df8e7767930d504ec0eeeb3c4ea
pasha-log <pasha.log@gmail.com>
2023-08-10 18:06:32 -0700

    changed socketIO

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = express()
 	.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server, {
+const io = require('socket.io')(server, {
 	cors: {
 		// origin: '*',
 		origin: 'https://instapost.herokuapp.com',
