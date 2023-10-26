2d3d54f951e419c485846b5a767c49ddce74d426
pasha-log <pasha.log@gmail.com>
2023-10-25 22:04:35 -0700

    making changes to server.js to fix SASL error

--- server.js
+++ server.js
@@ -41,13 +41,13 @@ require('dotenv').config();
 
 const express = require('express');
 
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('./client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	// Set static folder
+// 	app.use(express.static('./client/build'));
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
 
 server.listen(PORT, () => {
 	console.log(`Started on http://localhost:${PORT}`);
@@ -55,10 +55,24 @@ server.listen(PORT, () => {
 
 const io = require('socket.io')(server, {
 	cors: {
-		origin: 'https://pasha-log-instapost.surge.sh',
+		// origin: 'https://pasha-log-instapost.surge.sh',
+		origin: '*',
 		methods: [ 'GET', 'POST' ],
 		credentials: 'true'
 	}
+});
+
+const _dirname = path.dirname('');
+const buildPath = path.join(_dirname, '../Unit50Capstone2Frontend/build');
+
+app.use(express.static(buildPath));
+
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname, '../Unit50Capstone2Frontend/build/index.html'), function(err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+	});
 });
 
 io.on('connection', (socket) => {
